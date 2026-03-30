5a2643939fd71f48dc604d6b4a7aa49f11316177
Surajsahu2005 <kumarsahus538@gmail.com>
2026-03-30 17:30:50 +0530

    1st commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const Product = require("./models/Product");
+const Product = require("./models/product");
 const path = require("path");
 const User = require("./models/User");
 
